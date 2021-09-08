2808e7d1226505492bbd53e878d7190554834605
JenniferStohler <61611402+JenniferStohler@users.noreply.github.com>
2021-09-08 10:45:49 -0600

    navbar mostly done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <navbar />
+  </header>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import navbar from './components/navbar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    navbar
   }
 }
 </script>
---
+++ src/components/navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="d-flex flex-column align-items-left">
+      </div>
+    </router-link>
+     <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarText"
+      aria-controls="navbarText"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <router-link :to="{ name: 'Home' }" class="nav-link">
+            Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'About' }" class="nav-link">
+            About
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link :to="{ name: 'Commissions' }" class="nav-link">
+            Commissions
+          </router-link>
+        </li>
+        <li>
+          <input type="text">
+          <button type="submit" class="btn btn-light btn-sm">Search</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.dropdown-menu {
+  user-select: none;
+  display: block;
+  transform: scale(0);
+  transition: all 0.15s linear;
+}
+.dropdown-menu.show {
+  transform: scale(1);
+}
+.hoverable {
+  cursor: pointer;
+}
+a:hover {
+  text-decoration: none;
+}
+.nav-link{
+  text-transform: uppercase;
+}
+.nav-item .nav-link.router-link-exact-active{
+  color: var(--danger);
+}
+
+</style>
